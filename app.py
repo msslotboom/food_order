ed31ebb3d8352d6be18d4f6062e94ab6184b7137
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2023-06-11 13:43:21 +0300

    Added basic form and basic display of filled in form

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask
+from flask import render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Heipparallaa!"
+    restaurants = ["Pizzeria", "Sushi restaurant", "Cafe"]
+    return render_template("index.html", restaurants=restaurants)
+
+@app.route("/order")
+def order():
+    return render_template("order.html")
+
+@app.route("/order", methods=["POST"])
+def process_order():
+    pizza = request.form["pizza"]
+    message = request.form["message"]
+    return render_template("result.html", pizza=pizza,
+                                          message=message)
