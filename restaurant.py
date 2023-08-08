ee183502fe31791941f3879fbf8e2527f954b2df
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2023-08-08 19:35:39 +0300

    Added function to check whether user is restaurant owner

--- restaurant.py
+++ restaurant.py
@@ -15,3 +15,10 @@ def get_restaurants_from_owner_id(user_id):
     sql = ("SELECT * FROM restaurants WHERE owner_id=:owner_id")
     restaurant_id = db.session.execute(text(sql), {"owner_id":user_id}).fetchall()
     return restaurant_id
+
+def user_is_restaurant_owner(user_id, restaurant_id):
+    sql = ("SELECT owner_id FROM restaurants WHERE id=:restaurant_id")
+    owner_id = db.session.execute(text(sql), {"restaurant_id":restaurant_id}).fetchone()[0]
+    if owner_id == user_id:
+        return True
+    return False
